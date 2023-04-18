8b0e79ab513dfcc07a3ba1b6801d1252a33d54fd
karthik <karthikitech>
2023-04-18 11:46:16 +0530

    First Commit

---
+++ Task15/src/org/test/PatternPrinting.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class PatternPrinting {
+	public static void main(String[] args) {
+		for (int i = 1; i <=7; i++) {
+			System.out.print("*");
+	    for (int j = 1; j <=i; j++) {
+	    	System.out.print(j);
+				
+		}
+	   }
+	}
+
+}
