a4bc350c811ad0e87a262b4260d0c5e539ab1895
karthik <karthikitech>
2023-04-18 11:50:51 +0530

    First Commit

---
+++ Task16/src/org/test/ParentClass1.java
@@ -0,0 +1,7 @@
+package org.test;
+
+public interface ParentClass1 {
+	void test1();
+	void test2();
+
+}
---
+++ Task16/src/org/test/ParentClass2.java
@@ -0,0 +1,6 @@
+package org.test;
+
+public interface ParentClass2 {
+	void test3();
+	  void test4();
+}
