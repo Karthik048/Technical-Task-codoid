c09bcb5eba761471d59b86de0e129cd1be95c7ba
karthik <karthikitech>
2023-04-18 10:56:04 +0530

    First Commit

---
+++ Task1/src/org/test/ReverseString.java
@@ -0,0 +1,14 @@
+//2.How do we reverse a string?
+package org.test;
+public class ReverseString {
+	public static void main(String[] args) {
+		String s="i am karthik";
+		String rev="";
+		for (int i = s.length()-1; i >=0; i--) {
+			rev=rev+s.charAt(i);
+		}
+		System.out.println(rev);
+	}
+
+
+}
