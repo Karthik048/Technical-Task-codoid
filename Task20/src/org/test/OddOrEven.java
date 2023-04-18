72ac1e0f40b047d59fa203d2fc1313bc3d7d38a2
karthik <karthikitech>
2023-04-18 12:06:32 +0530

    First Commit

---
+++ Task20/src/org/test/OddOrEven.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class OddOrEven {
+	public static void main(String[] args) {
+		int i=22;
+		if (i%2==0) {
+			System.out.println("Given Number is Even");	
+		} else {
+            System.out.println("Given Number is Odd");
+		}
+	}
+
+}
